3355a8a14e3d57962830f860102a65009e221f5c
engrpaim <joseephraimbatacan@gmail.com>
2024-10-09 17:33:08 +0800

    SMS DONE

---
+++ SS.ino
@@ -0,0 +1,337 @@
+#include "WiFiS3.h"
+#include "WiFiClient.h"
+#include "arduino_secrets.h"
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
+#include <ArduinoJson.h>
+#include <cstring>
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial sim800(8, 7);
+//CLASS FOR MATRIX DISPLAY
+ArduinoLEDMatrix matrix;
+
+class MessageDisplay {
+  private:
+    ArduinoLEDMatrix &matrix; 
+    String message;
+
+  public:
+  
+    MessageDisplay(ArduinoLEDMatrix &m) : matrix(m) {}
+
+
+    void setMessage(String msg) {
+      message = msg;
+    }
+
+    void displayStatic(const char* text) {
+      matrix.beginDraw();
+      matrix.stroke(0xFFFFFFFF);
+      matrix.textFont(Font_4x6);
+      matrix.beginText(0, 1, 0x00FF00);
+      matrix.println(text);
+      matrix.endText();
+      matrix.endDraw();
+    }
+};
+
+MessageDisplay display(matrix);
+
+// COMPONENTS VARIABLES
+
+const int modeSwitch = 3; 
+const int doortrigger = 4; 
+const int closeTrigger = 5; 
+
+
+//WIFI CREDENTIALS & METHODS
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+int status = WL_IDLE_STATUS;
+char server[] = "172.17.8.60";
+
+WiFiClient client;
+
+void printWifiStatus() 
+{
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI): ");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+//HTTP REQUEST RESPONSE
+
+void read_response() 
+{
+  while (client.available()) 
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+}
+//------------------------------------------------------------------CONTROL METHODS----------------------------------------------------------------------//
+void triggerDoor() 
+{
+  delay(1000);
+  digitalWrite(doortrigger, LOW);  
+  Serial.println("Door triggered");
+}
+void openDoor() 
+{
+  digitalWrite(doortrigger, HIGH);
+  if(digitalRead(closeTrigger) == LOW )
+  {
+    for(int i = 0; i <= 5; i++) {
+      Serial.println("Door triggered for " + String(i) + " sec");
+      String countDown = "O0"+String(i);
+      display.displayStatic(countDown.c_str()); 
+      delay(1000);
+      if(digitalRead(closeTrigger) == HIGH ){
+        Serial.println("Door Opened");
+        break;
+      }
+      if( i == 5 ){
+        isDataSaved();
+      }
+    }
+    
+  }
+  delay(1000);
+    
+  
+}
+//send message
+void sendSMS(String number, String message) {
+  sim800.print("AT+CMGS=\"");
+  sim800.print(number);
+  sim800.println("\"");
+  delay(100);
+
+  sim800.println(message);
+  delay(100);
+
+  sim800.write(26); // ASCII code of CTRL+Z to send the message
+  delay(100);
+
+  // Check for response
+  while (sim800.available()) {
+    Serial.write(sim800.read());
+  }
+}
+//RELAY TO SAVED CLOSED
+
+void isDataSaved() 
+{
+
+  if (WiFi.status() == WL_NO_MODULE) 
+  {
+    Serial.println("Communication with WiFi module failed!");
+    while (true);
+  }
+
+  unsigned long startTime = millis();
+  while (status != WL_CONNECTED) 
+  {
+    if (millis() - startTime > 10000) { // 10 seconds timeout
+      Serial.println("Connection timed out!");
+      return;
+    }
+    display.displayStatic("::con");
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(5000);
+  }
+
+  printWifiStatus();
+  Serial.println("\nStarting connection to server...");
+  if (client.connect(server, 80)) 
+  {
+    Serial.println("Connected to server");
+    String postData = "status=CLOSED";
+    client.println("POST /refx/controllers/data_results.php HTTP/1.1");
+    client.println("Host: 172.17.8.60");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println("Connection: close");
+    client.println();
+    client.print(postData);
+
+    read_response();
+    
+   
+  }
+  else 
+  {
+    display.displayStatic("Err1");
+    Serial.println("Connection to server failed!");
+  }
+}
+
+
+
+// CHECK STATUS EVERY 5 SECONDS
+
+void checkStatus()
+  {
+   if (WiFi.status() == WL_NO_MODULE) 
+    {
+      Serial.println("Communication with WiFi module failed!");
+      while (true);
+    }
+
+  
+  
+
+    if (client.connect(server, 80)) 
+      { 
+        client.println("GET /refx/fetch_status.php HTTP/1.1"); 
+        client.println("Host: 172.17.8.60"); 
+        client.println("Connection: close");
+        client.println();
+
+        while (client.connected() || client.available()) 
+          {
+            if (client.available()) 
+              { 
+                String json = client.readStringUntil('\n');
+                StaticJsonDocument<200> doc;
+                deserializeJson(doc, json);
+
+                
+                const char* example = doc[0]["status"]; // Adjust as needed
+                Serial.println(example);
+
+                if (strcmp(example, "OPEN") == 0) 
+                  {
+                    Serial.println("OPEN HELLO WORLD");
+                     openDoor();
+                  }
+
+                if (strcmp(example, "NEXT") == 0) 
+                  { 
+                   
+                    Serial.println("NEXT HELLO WORLD");
+                     if(digitalRead(closeTrigger) == LOW )
+                      {
+                        for(int i = 0; i <= 3; i++) 
+                        {
+                          Serial.println("Door triggered for " + String(i) + " sec");
+                          String countDown = "N0"+String(i);
+                          display.displayStatic(countDown.c_str()); 
+                          delay(1000);
+
+                          if( i == 3 )
+                          {
+                            triggerDoor();
+                          }
+                        }
+    
+                      }
+                    
+                    
+                  }
+              } 
+          }
+        client.stop();
+      }  
+      else 
+      {
+        Serial.println("Connection failed");
+      }
+
+    delay(1000);
+  }
+
+void setup() 
+  {
+
+    pinMode(doortrigger, OUTPUT); 
+    pinMode(closeTrigger, INPUT_PULLUP); 
+    pinMode(modeSwitch, INPUT_PULLUP);
+    digitalWrite(doortrigger, HIGH);
+    
+    
+    Serial.begin(115200);
+    sim800.begin(115200);
+    matrix.begin();  
+ 
+    display.displayStatic("REFX ");
+    delay(2000);
+    display.displayStatic("%AE");
+    delay(2000);
+    display.displayStatic("%24");
+    delay(2000);
+    while (!Serial) { ; }
+    unsigned long startTime = millis();
+    while (status != WL_CONNECTED) {
+    if (millis() - startTime > 10000) 
+    { 
+      Serial.println("Connection timed out!");
+      
+      return;
+    }
+    display.displayStatic("EI1");
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(4000);
+
+    sim800.println("AT");
+    delay(100);
+    while (sim800.available())
+    {
+      Serial.write(sim800.read());
+    }
+
+  
+    sim800.println("AT+CMGF=1");
+    delay(100);
+
+    Serial.println("Sending SMS...");
+    sendSMS("+639217293658", "---- P5 REFMONITORING ---- \n  \n[{ SYSTEM IS ONLINE }]\n\n\n This SMS is auto generated by the system \n\n---- AUTOMATION 2024 ----" );
+      delay(1000);
+  }
+  
+
+
+  }
+
+void loop() 
+{
+ 
+  if(status == WL_CONNECTED)
+  {     
+
+      if (digitalRead(modeSwitch) == HIGH) 
+      {
+          checkStatus();
+          display.displayStatic("P01");
+          Serial.println("P01");
+          delay(1000);
+      } 
+      else 
+      {
+
+          display.displayStatic("P02");
+          Serial.println("P02 No program.");
+          delay(5000);
+    
+      }
+  }
+  else
+  {
+    display.displayStatic("E01");
+  }
+}
