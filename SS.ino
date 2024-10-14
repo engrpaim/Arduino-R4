56d6789eb5fb6f8f817aaa646c2a72fc510ab3b5
engrpaim <joseephraimbatacan@gmail.com>
2024-10-14 16:00:49 +0800

    sms notification

--- SS.ino
+++ SS.ino
@@ -46,16 +46,22 @@ const int modeSwitch = 3;
 const int doortrigger = 4;
 const int closeTrigger = 5;
 const int temperatureAlarm = 9;
-
-
-
+const float pt1000Pin = A0;
+const float potentionmeter = A1;
+const float R0 = 10000.0;  // Resistance at 0°C for PT1000
+const float R1 = 10000.0;  // Resistor for voltage
+const float alpha = 0.00385;
+bool sensorTemperature = false;
+const int soundAlaram = 11;
+const int calibrate = 13;
+int lastCalibrate = 0;
 //WIFI CREDENTIALS & METHODS
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 int checkOnline = 0;
 int status = WL_IDLE_STATUS;
-char server[] = "172.17.8.60";
+char server[] = "172.17.2.236";
 
 WiFiClient client;
 
@@ -119,6 +125,7 @@ void sendSMS(String number, String message) {
   sim800.println(message);
   delay(100);
 
+
   sim800.write(26);  // ASCII code of CTRL+Z to send the message
   delay(100);
 
@@ -156,7 +163,7 @@ void isDataSaved() {
     Serial.println("Connected to server");
     String postData = "status=CLOSED";
     client.println("POST /refx/controllers/data_results.php HTTP/1.1");
-    client.println("Host: 172.17.8.60");
+    client.println("Host: 172.17.2.236");
     client.println("Content-Type: application/x-www-form-urlencoded");
     client.print("Content-Length: ");
     client.println(postData.length());
@@ -174,6 +181,66 @@ void isDataSaved() {
 }
 
 
+//
+void isTempSaved(String currentTemp) {
+
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    while (true)
+      ;
+  }
+
+  unsigned long startTime = millis();
+  while (status != WL_CONNECTED) {
+    if (millis() - startTime > 10000) {  // 10 seconds timeout
+      Serial.println("Connection timed out!");
+      return;
+    }
+    display.displayStatic("::con");
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(5000);
+  }
+
+  printWifiStatus();
+  Serial.println("\nStarting connection to server...");
+  if (client.connect(server, 80)) {
+    Serial.println("Connected to server");
+    String postData = currentTemp;
+    client.println("POST /refx/controllers/data_results.php HTTP/1.1");
+    client.println("Host: 172.17.2.236");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println("Connection: close");
+    client.println();
+    client.print(postData);
+
+    read_response();
+
+
+  } else {
+    display.displayStatic("Err1");
+    Serial.println("Connection to server failed!");
+  }
+}
+
+void postTemperature() {
+  String postTemp;
+  float currentTemperature = temperaturedx();
+  String temperatureMessage = "Send Temp: " + String(currentTemperature) + " °C";
+  if (currentTemperature != -1) {  // Check if temperature reading is valid
+
+    postTemp = "temperature=" + String(currentTemperature);
+    Serial.println(postTemp);
+    isTempSaved(postTemp);
+  } else {
+    postTemp = "temperature=error";
+    Serial.println(postTemp);
+    isTempSaved(postTemp);
+  }
+}
 
 // CHECK STATUS EVERY 5 SECONDS
 
@@ -183,7 +250,10 @@ void checkStatus() {
   if (checkOnline == 10000) {
     groupMessage("+639217293658", "REF MONITORING IS ONLINE");
     checkOnline = 0;
+  } else if (checkOnline % 2 == 0) {
+    postTemperature();
   }
+
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     while (true)
@@ -195,7 +265,7 @@ void checkStatus() {
 
   if (client.connect(server, 80)) {
     client.println("GET /refx/fetch_status.php HTTP/1.1");
-    client.println("Host: 172.17.8.60");
+    client.println("Host: 172.17.2.236");
     client.println("Connection: close");
     client.println();
 
@@ -217,16 +287,16 @@ void checkStatus() {
         }
 
         if (strcmp(status, "NEXT") == 0) {
-
+          digitalWrite(doortrigger, LOW);
           Serial.println("NEXT HELLO WORLD");
           if (digitalRead(closeTrigger) == LOW) {
-            for (int i = 0; i <= 3; i++) {
-              Serial.println("Door triggered for " + String(i) + " sec");
-              String countDown = "N0" + String(i);
+            for (int o = 0; o <= 3; o++) {
+              Serial.println("Door triggered for " + String(o) + " sec");
+              String countDown = "N0" + String(o);
               display.displayStatic(countDown.c_str());
               delay(1000);
 
-              if (i == 3) {
+              if (o == 3) {
                 triggerDoor();
               }
             }
@@ -277,7 +347,7 @@ void getSMSManager(int messageNumber) {
 
   if (client.connect(server, 80)) {
     client.println("GET /refx/fetch_sms.php HTTP/1.1");
-    client.println("Host: 172.17.8.60");
+    client.println("Host: 172.17.2.236");
     client.println("Connection: close");
     client.println();
 
@@ -310,6 +380,7 @@ void getSMSManager(int messageNumber) {
                 delay(6000);
                 break;
               case 2:
+                // 1 decimal place
                 message = "---- P5 REFMONITORING ---- \n  \n[{ WARNING: TEMPERATURE IS ABOVE TARGET!! }]\n\n SENT TO " + Recipient + " \n\n This SMS is automatically generated by the system \n\n---- AUTOMATION 2024 ----";
                 groupMessage(sentNumber, message);
                 delay(6000);
@@ -332,13 +403,112 @@ void getSMSManager(int messageNumber) {
     display.displayStatic("WFX");
   }
 }
+
+void temperature() {
+  digitalWrite(soundAlaram, LOW);
+  float potValue = analogRead(potentionmeter);
+  float calibrationOffset = (potValue);
+
+
+
+  float sensorValue = analogRead(pt1000Pin);
+  float offset = analogRead(potentionmeter);
+  float voltage = (sensorValue / 1023.0) * 5.0;
+
+
+
+  if (voltage <= 0 || voltage >= 5.0) {
+    Serial.println("Invalid voltage reading.");
+    return;
+  }
+
+
+  float resistance = (R1 * (5.0 - voltage)) / voltage;
+
+  if (resistance <= 0) {
+    Serial.println("Invalid resistance value.");
+    return;
+  }
+
+
+  float temperature = (resistance / R0 - 1) / alpha;
+
+
+
+  temperature = temperature + potValue - 13;
+
+  if (temperature > 30) {
+
+    sensorTemperature = true;
+
+  } else {
+
+    sensorTemperature = false;
+  }
+
+  //sensorValue
+  Serial.print("Resistance: ");
+  Serial.println(calibrationOffset);
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  delay(1000);
+}
+
+float temperaturedx() {
+  float potValue = analogRead(potentionmeter);
+  float calibrationOffset = (potValue);
+
+  float sensorValue = analogRead(pt1000Pin);
+  float voltage = (sensorValue / 1023.0) * 5.0;
+
+  if (voltage <= 0 || voltage >= 5.0) {
+    Serial.println("Invalid voltage reading.");
+    return -1;  // Indicate error
+  }
+
+  float resistance = (R1 * (5.0 - voltage)) / voltage;
+
+  if (resistance <= 0) {
+    Serial.println("Invalid resistance value.");
+    return -1;  // Indicate error
+  }
+
+  float temperature = (resistance / R0 - 1) / alpha;
+  temperature = temperature + potValue - 10;
+
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+
+  return temperature;  // Return the temperature
+}
+
+void soundBlinking() {
+
+  for (int i = 0; i <= 3; i++) {
+    digitalWrite(soundAlaram, HIGH);
+    delay(500);
+    digitalWrite(soundAlaram, LOW);
+    delay(500);
+    digitalWrite(soundAlaram, HIGH);
+    delay(1000);
+    digitalWrite(soundAlaram, LOW);
+    delay(500);
+    digitalWrite(soundAlaram, HIGH);
+    delay(500);
+    digitalWrite(soundAlaram, LOW);
+    delay(500);
+  }
+}
 void setup() {
 
   pinMode(doortrigger, OUTPUT);
   pinMode(closeTrigger, INPUT_PULLUP);
   pinMode(modeSwitch, INPUT_PULLUP);
   pinMode(temperatureAlarm, INPUT_PULLUP);
-  digitalWrite(doortrigger, HIGH);
+  pinMode(calibrate, INPUT_PULLUP);
+  digitalWrite(doortrigger, LOW);
 
 
   Serial.begin(115200);
@@ -380,29 +550,82 @@ void setup() {
     // sendSMS("+639217293658", "---- P5 REFMONITORING ---- \n  \n[{ SYSTEM IS ONLINE }]\n\n\n This SMS is auto generated by the system \n\n---- AUTOMATION 2024 ----");
     delay(1000);
   }
+  pinMode(soundAlaram, OUTPUT);
+  digitalWrite(soundAlaram, HIGH);
+  delay(500);
 }
 
 void loop() {
-  if (digitalRead(temperatureAlarm) == HIGH) {
-    if (status == WL_CONNECTED) {
+  if (digitalRead(calibrate) == LOW) {
+    if (lastCalibrate == 1) {
+      Serial.println("CALIBRATED");
+      lastCalibrate = 0;
+    }
+    temperature();
+    if (digitalRead(modeSwitch) == HIGH) {
+      if (sensorTemperature == false) {
 
-      if (digitalRead(modeSwitch) == HIGH) {
-        checkStatus();
-        display.displayStatic("P01");
-        Serial.println("P01");
-        delay(1000);
+        if (digitalRead(temperatureAlarm) == HIGH) {
+          if (status == WL_CONNECTED) {
+
+
+            checkStatus();
+            display.displayStatic("P01");
+            Serial.println("P01");
+            delay(1000);
+
+          } else {
+            display.displayStatic("E01");
+          }
+        } else {
+          postTemperature();
+          
+          display.displayStatic("T%2");
+          Serial.println("HIGH TEMPERATURE!!");
+          getSMSManager(2);
+          alarmDoor();
+          soundBlinking();
+          delay(1000);
+        }
       } else {
-
-        display.displayStatic("P02");
-        Serial.println("P02 No program.");
-        delay(5000);
+        postTemperature();
+        soundBlinking();
+        display.displayStatic("T%1");
+        Serial.println("HIGH TEMP!");
+        getSMSManager(2);
+        alarmDoor();
+        soundBlinking();
+        delay(1000);
       }
     } else {
-      display.displayStatic("E01");
+      if (digitalRead(temperatureAlarm) == HIGH) {
+        if (status == WL_CONNECTED) {
+
+
+          checkStatus();
+          display.displayStatic("P02");
+          Serial.println("P02");
+          delay(1000);
+
+        } else {
+          display.displayStatic("E01");
+        }
+      } else {
+        postTemperature();
+        display.displayStatic("T%2");
+        Serial.println("HIGH TEMPERATURE!!");
+        getSMSManager(2);
+        alarmDoor();
+        soundBlinking();
+        delay(1000);
+      }
     }
   } else {
-    Serial.println("HIGH TEMPERATURE!!");
-    getSMSManager(2);
+    lastCalibrate = 1;
+    float tempDisplay = temperaturedx();
+    String readytempDisplay = String(tempDisplay);  // Convert float to String
+    display.displayStatic(readytempDisplay.c_str());
     delay(1000);
+    digitalWrite(soundAlaram, LOW);
   }
 }
